cae5edba855176829d6a5f26095d3daef9f06287
khemlall mangal <randi2160@yahoo.com>
2022-08-08 23:32:23 -0400

    adding additional login mechnism for UI

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+model: any ={}
+LoggedIn: boolean;
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+  }
+  
+  login(){
+    //console.log()
+    this.accountService.login(this.model).subscribe(response =>{
+  console.log(response);
+  this.LoggedIn=true;
+    },error => {
+      console.log(error);
+    })
+  }
+
+
+  logout(){
+    //console.log()
+    this.LoggedIn=false;
+
+   
   }
 
 }
